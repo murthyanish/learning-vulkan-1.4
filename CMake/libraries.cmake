f89119747ada5e8a35dc6cc22509d7344d9d88a1
Anish Murthy <anish.murthy.dev@gmail.com>
2026-06-24 23:07:05 -0700

    Vulkan::cppm working

--- CMake/libraries.cmake
+++ CMake/libraries.cmake
@@ -3,13 +3,81 @@ cmake_minimum_required(VERSION 4.3)
 include(FetchContent)
 
 find_package (glm REQUIRED)
-find_package (VulkanHpp 1.4.335 REQUIRED)      # Require Vulkan SDK version 1.4.335 or higher
+find_package (Vulkan 1.4.335 REQUIRED)      # Require Vulkan SDK version 1.4.335 or higher
 find_package (tinyobjloader REQUIRED)
 find_package (tinygltf REQUIRED)
 find_package (KTX REQUIRED)
 
+# set up Vulkan C++ module only if enabled
+if(ENABLE_CPP23_MODULE)
+  add_library(VulkanCppModule)
+  add_library(Vulkan::cppm ALIAS VulkanCppModule)
+
+  target_compile_definitions(VulkanCppModule
+          PUBLIC
+            VULKAN_HPP_DISPATCH_LOADER_DYNAMIC=1
+            VULKAN_HPP_NO_STRUCT_CONSTRUCTORS=1
+            VULKAN_HPP_CXX_MODULE_EXPERIMENTAL_WARNING=1
+  )
+  target_include_directories(VulkanCppModule
+          PUBLIC
+          "${Vulkan_INCLUDE_DIR}"
+  )
+  target_link_libraries(VulkanCppModule
+          PUBLIC
+          Vulkan::Vulkan
+  )
+
+  set_target_properties(VulkanCppModule PROPERTIES
+    CXX_STANDARD 23
+    CXX_STANDARD_REQUIRED YES
+    CXX_MODULE_STD ON
+  )
+
+  # Add MSVC-specific compiler options for proper C++ module support
+  if(MSVC)
+    target_compile_options(VulkanCppModule PRIVATE
+      /std:c++latest      # Use latest C++ standard for better module support
+      /permissive-        # Standards conformance mode
+      /Zc:__cplusplus     # Enable correct __cplusplus macro
+      /EHsc               # Enable C++ exception handling
+      /Zc:preprocessor    # Use conforming preprocessor
+      /translateInclude   # Automatically translate #include to import for standard library
+    )
+  endif()
+
+  target_sources(VulkanCppModule
+          PUBLIC
+          FILE_SET cxx_modules TYPE CXX_MODULES
+          BASE_DIRS
+          "${Vulkan_INCLUDE_DIR}"
+          FILES
+          "${Vulkan_INCLUDE_DIR}/vulkan/vulkan.cppm"
+  )
+
+
+  # # Add the vulkan.cppm file directly as a source file
+  # target_sources(VulkanCppModule
+  #         PRIVATE
+  #         "${Vulkan_INCLUDE_DIR}/vulkan/vulkan.cppm"
+  # )
+else()
+  # Create a dummy interface library when C++ 20 module is disabled
+  add_library(VulkanCppModule INTERFACE)
+  add_library(Vulkan::cppm ALIAS VulkanCppModule)
+  target_link_libraries(VulkanCppModule INTERFACE Vulkan::Vulkan)
+  target_compile_definitions(VulkanCppModule
+          INTERFACE VULKAN_HPP_DISPATCH_LOADER_DYNAMIC=1 VULKAN_HPP_NO_STRUCT_CONSTRUCTORS=1
+  )
+endif()
+
 find_package(stb REQUIRED)
 set(STB_INCLUDEDIR ${stb_INCLUDE_DIRS})
+
+add_executable (glslang::validator IMPORTED)
+find_program (GLSLANG_VALIDATOR "glslangValidator" HINTS $ENV{VULKAN_SDK}/bin REQUIRED)
+set_property (TARGET glslang::validator PROPERTY IMPORTED_LOCATION "${GLSLANG_VALIDATOR}")
+find_program(SLANGC_EXECUTABLE slangc HINTS $ENV{VULKAN_SDK}/bin REQUIRED)
 
 # This takes care of vendored libraries. Also has code to use library libs
 # instead if LVULKAN_<lib>_VENDORED is set to false.
@@ -18,7 +86,7 @@ add_subdirectory(${CMAKE_SOURCE_DIR}/vendored)
 add_library(lvulkan_libraries INTERFACE)
 target_link_libraries(lvulkan_libraries
   INTERFACE
-    VulkanHpp::VulkanHpp
+    Vulkan::cppm
     lvulkan_vendored
     glm::glm
 )
